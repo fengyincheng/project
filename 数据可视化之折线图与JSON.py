1dc72c1b3d8e909a6c2e11d3fe76d0f2fb0ccabc
fengyincheng <3035714895@qq.com>
2025-10-30 22:28:19 +0800

    豪华plus版

--- 数据可视化之折线图与JSON.py
+++ 数据可视化之折线图与JSON.py
@@ -10,10 +10,24 @@
 
 
 from pyecharts.charts import Line
+from pyecharts.options import TitleOpts,LegendOpts,ToolboxOpts,VisualMapOpts
 line = Line()
 #X轴数据
 line.add_xaxis(["中国","美国","英国"])
 #Y轴数据
 line.add_yaxis("GDP",[300,50,20])
 #通过render方法生成图表
+
+#增加全局配置项
+line.set_global_opts(
+    title_opts=TitleOpts(title = "GTP对比图",pos_left="center",pos_bottom="1%"),#标题
+    legend_opts=LegendOpts(is_show=True),#图例
+    toolbox_opts=ToolboxOpts(is_show=True),#工具栏
+    visualmap_opts=VisualMapOpts(is_show=True))#视觉映射
+
+
+
+
+
+
 line.render()
