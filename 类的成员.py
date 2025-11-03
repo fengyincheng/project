3062d18a5e09872baaea8f655d7a2abf53a73d2a
fengyincheng <3035714895@qq.com>
2025-11-03 18:38:19 +0800

    .

---
+++ 类的成员.py
@@ -0,0 +1,11 @@
+class Student:
+    name = None
+    def say_hi(self,msg):
+        print(f"hello everyone,I am {self.name},{msg}")
+stu = Student() #括号
+stu.name = 'lily'
+stu.say_hi('If you fuck my brother,I will kill you') #括号
+
+stu2 = Student() #括号
+stu2.name = 'Andy'
+stu2.say_hi('shot out!Alshyli') #括号
